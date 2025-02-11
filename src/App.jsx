d62e368fa398904e6183a8b7b183dad7f0acc687
Mogehs <mugheesurrehman06@gmail.com>
2025-02-11 11:30:49 +0500

    setup2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
+const MainLayout = () => (
+  <div className="lg:mx-auto lg:max-w-7xl">
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </div>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/services", element: <Services /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/about-us", element: <About /> },
+      { path: "/contact-us", element: <Contact /> },
+    ],
+  },
+]);
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
